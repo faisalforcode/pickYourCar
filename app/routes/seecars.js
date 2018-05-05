9a6153f9d437b2f87575600e470c03386d9d3056
Faisal Khan <faisalkhan@techgeek.co.in>
2018-05-04 23:53:42 -0700

    Added new route buy

--- app/routes/seecars.js
+++ app/routes/seecars.js
@@ -12,8 +12,8 @@ export default Route.extend({
       image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
       description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
     }, {
-      id: 'urban-living',
-      title: 'Urban Living',
+      id: 'mercedes',
+      title: 'Mercedes',
       owner: 'Mike TV',
       city: 'Seattle',
       category: 'Condo',
@@ -21,8 +21,8 @@ export default Route.extend({
       image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
       description: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
     }, {
-      id: 'downtown-charm',
-      title: 'Downtown Charm',
+      id: 'honda',
+      title: 'Honda',
       owner: 'Violet Beauregarde',
       city: 'Portland',
       category: 'Apartment',
