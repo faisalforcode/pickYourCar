f08dfcd4fe556b2522cc370e3a560c98fa7ed4f6
choushruti88 <chou.shruti@gmail.com>
2018-05-08 01:12:12 -0700

    Buy Contact logo template

--- app/routes/seecars.js
+++ app/routes/seecars.js
@@ -1,33 +1,36 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-	 model() {
+  model() {
     return [{
       id: 'audi',
       title: 'Audi Q6',
       owner: 'Veruca Salt',
       city: 'San Francisco',
-      category: 'Estate',
-      bedrooms: 15,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+      category: 'Used',
+      type: "Sedan",
+      price: 15000,
+      image: 'http://2018hybridcars.com/wp-content/uploads/2017/08/jjhh.jpg',
       description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
     }, {
       id: 'mercedes',
       title: 'Mercedes',
       owner: 'Mike TV',
       city: 'Seattle',
-      category: 'Condo',
-      bedrooms: 1,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
+      category: 'Used',
+      type: "Sedan",
+      price: 16000,
+      image: 'https://assets.mbusa.com/vcm/MB/DigitalAssets/Vehicles/Models/2018/E300W/Features/2018-E-CLASS-SEDAN-011-MCFO.jpg',
       description: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
     }, {
       id: 'honda',
-      title: 'Honda',
+      title: 'Honda Civic',
       owner: 'Violet Beauregarde',
       city: 'Portland',
-      category: 'Apartment',
-      bedrooms: 3,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
+      category: 'Used',
+      type: "Hatchback",
+      price: 12000,
+      image: 'https://www.carscoops.com/wp-content/uploads/2016/09/2017-honda-civic-us-pricing-14.jpg',
       description: 'Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet.'
     }];
   }
