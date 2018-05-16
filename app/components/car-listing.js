2476e1c8ae2bb51f195e44f53efcec548f1a74f6
choushruti88 <chou.shruti@gmail.com>
2018-05-15 18:36:01 -0700

    noimagebkgnd

--- app/components/car-listing.js
+++ app/components/car-listing.js
@@ -2,9 +2,14 @@ import Component from '@ember/component';
 
 export default Component.extend({
 	isWide: false,
+	classNames: ['dib'],
+  isClicked: false,
   actions: {
     toggleImageSize() {
       this.toggleProperty('isWide');
-    }
+    },
+		showMenu: function(){
+			this.toggleProperty('isClicked');
+		}
   }
 });
